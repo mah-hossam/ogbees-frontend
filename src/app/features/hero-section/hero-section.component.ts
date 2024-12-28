dfe8fa593dc6f94023d19a49a875009fd2df39b4
unknown <mahmoud-hossam@outlook.com>
2024-12-28 17:53:23 +0200

    hero section -without animation for sm screens

---
+++ src/app/features/hero-section/hero-section.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../../shared/navbar/navbar.component";
+
+@Component({
+  selector: 'app-hero-section',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './hero-section.component.html',
+  styleUrl: './hero-section.component.scss'
+})
+export class HeroSectionComponent {
+
+}
